5f5c30149b55af49cf332a236977dd4befabf2e2
varin <zvv1287@mail.ru>
2021-03-23 20:15:54 +0300

    добавление заказа в админку

--- ordersapp/admin.py
+++ ordersapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from ordersapp.models import Order, OrderItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'updated', 'status', 'is_active')
+    list_display_links = ('user',)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
+    list_display_links = ('order', 'product')
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
